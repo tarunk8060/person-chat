706dccea78ef93dbf69ad27e2cb19f6b1a65020d
tarunk8060 <kstarun8246@gmail.com>
2026-06-19 09:41:04 +0530

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,12 +1,16 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import streamlit as st
 
 load_dotenv()
 
-client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
+api_key = os.getenv("GEMINI_API_KEY")
+
+if not api_key:
+    api_key = st.secrets["GEMINI_API_KEY"]
+
+client = genai.Client(api_key=api_key)
 
 def generate_response(persona, query, context):
     context_text = "\n".join(context[0])
